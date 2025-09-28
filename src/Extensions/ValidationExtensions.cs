40bc4c282c752a7a8d3a6b5a721c941e16258cee
Rafael Renault Andrade <rafynha.and@gmail.com>
2025-09-28 18:07:42 +0100

    New version 1.3.1 - Add Validation Extension DI registration

--- src/Extensions/ValidationExtensions.cs
+++ src/Extensions/ValidationExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Aurora.Mediator.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,9 +9,25 @@ public static class ValidationExtensions
     /// <summary>
     /// Ativa o ValidationBehavior no pipeline do Aurora.Mediator.
     /// </summary>
-    public static IServiceCollection UseDefaultValidation(this IServiceCollection services)
+    public static IServiceCollection UseDefaultValidation(this IServiceCollection services, params Assembly[] assembliesWithValidators)
     {
         services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+         if (assembliesWithValidators != null && assembliesWithValidators.Length > 0)
+        {
+            foreach (var assembly in assembliesWithValidators)
+            {
+                var validatorTypes = assembly.GetTypes()
+                    .Where(t => !t.IsAbstract && !t.IsInterface)
+                    .SelectMany(t => t.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                    .Where(x => x.Interface.IsGenericType && x.Interface.GetGenericTypeDefinition() == typeof(IValidator<>));
+
+                foreach (var v in validatorTypes)
+                {
+                    services.AddScoped(v.Interface, v.Type);
+                }
+            }
+        }
         return services;
     }
 }
